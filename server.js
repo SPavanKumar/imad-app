e6638bc5a0b4aed69ea35da174b4dcc5aa69696b
SPavanKumar <30950693+SPavanKumar@users.noreply.github.com>
2017-08-18 15:32:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/pavan', function (req, res) {
-   res.send('Hello...Harika...This is Pavan') ;
+   res.send('Hello...Harika...This is Pavan... now remove pavan in url and add harika') ;
 });
 
 app.get('/harika', function (req, res) {

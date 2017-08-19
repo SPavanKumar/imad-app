e3c20fa9c05220ebc74a895c3dcb063829e60a8b
SPavanKumar <30950693+SPavanKumar@users.noreply.github.com>
2017-08-19 15:52:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png',function (req, res) {
 });
 
 app.get('/ui/pavan.html',function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui','pavan.html');
+    res.sendFile(path.join(__dirname, 'ui','pavan.html'));
 });
 
 

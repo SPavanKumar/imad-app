3636bb1e1ce63da5a98daf0f6d5351cce9f7b212
SPavanKumar <30950693+SPavanKumar@users.noreply.github.com>
2017-08-21 17:53:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ var Pro1 = {
     title: 'ProRss',
     heading: 'Mobile app',
     date : 'aug12,15',
-    content: `<p> Hi this is Pavan Kumar. I am going to develop application for Rss People </p>
-              <p> The app will be very useful for fruite marketing and people well get fruites just with one tap</p>
-              <p> Happy to get this work and feeling proud to serve RSS.</p>`
+    content: `<p> This is my first html file.I am learnig html in this course. Click on "home".</p>
+        
+        <p> On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks. When you create pictures, charts, or diagrams, they also coordinate with your current document look. You can easily change the formatting of selected text in the document text by choosing a look for the selected text from the Quick Styles gallery on the Home tab. You can also format text directly by using the other controls on the Home tab. Most controls offer a choice of using the look from the current theme or using a format that you specify directly.</p>
+        <p>
+            To change the overall look of your document, choose new Theme elements on the Page Layout tab. To change the looks available in the Quick Style gallery, use the Change Current Quick Style Set command. Both the Themes gallery and the Quick Styles gallery provide reset commands so that you can always restore the look of your document to the original contained in your current template. On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks. When you create pictures, charts, or diagrams, they also coordinate with your current document look.
+        </p>
+         <p> selected text from the Quick Styles gallery on the Home tab. You can also format text directly by using the other controls on the Home tab. Most controls offer a choice of using the look from the current theme or using a format that you specify directly. To change the overall look of your document, choose new Theme elements on the Page Layout tab. To change the looks available in the Quick Style gallery, use the Change Current Quick Style Set command. Both the Themes gallery and the Quick Styles gallery provide reset commands so that you can always restore the look of your document to the original contained in your current template.
+        </p> `
 };
   
 function createTemplate(data) {
@@ -21,31 +26,31 @@ function createTemplate(data) {
     var date = data.date;
     var content = data.content;
     
-    var htmlTemplat = ` <html>
+    var htmlTemplate = ` <html>
                         <head>
                         <title>
                            ${title}
                         </title>
                         <meta name="viewport" content="width=device-width, initial-scale=1"/>
                         <link href="/ui/style.css" rel="stylesheet" />
-                    </head>
-                    <body>
-                        <h1>${heading}</h1>
-                        
-                      <div class=abc>
+                        </head>
+                        <body>
+                            <h1>${heading}</h1>
                             
-                        <div>
-                            <a href="/">Home</a>
-                        </div> 
-                        <hr/>
-                        <div>${date}</div>
-                        <div>
-                          ${content}
-                        </div>
-                       </div>
-                    </body>
-                    </html>
-                    `;
+                          <div class=abc>
+                                
+                            <div>
+                                <a href="/">Home</a>
+                            </div> 
+                            <hr/>
+                            <div>${date}</div>
+                            <div>
+                              ${content}
+                            </div>
+                           </div>
+                        </body>
+                        </html>
+                        `;
                     return htmlTemplate;
 }
 

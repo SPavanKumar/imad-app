595361cd30f8f0ef5ca6a0a1684426e6b935c76d
SPavanKumar <30950693+SPavanKumar@users.noreply.github.com>
2017-08-21 18:06:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ var Pro1 = {
     date : 'aug12,15',
     content: `<p> This is my first html file.I am learnig html in this course. Click on "home".</p>
         
-        <p> On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks. When you create pictures, charts, or diagrams, they also coordinate with your current document look. You can easily change the formatting of selected text in the document text by choosing a look for the selected text from the Quick Styles gallery on the Home tab. You can also format text directly by using the other controls on the Home tab. Most controls offer a choice of using the look from the current theme or using a format that you specify directly.</p>
+        <p>Hi this is Pavan. I am going to create a mobile app.</p>
         <p>
-            To change the overall look of your document, choose new Theme elements on the Page Layout tab. To change the looks available in the Quick Style gallery, use the Change Current Quick Style Set command. Both the Themes gallery and the Quick Styles gallery provide reset commands so that you can always restore the look of your document to the original contained in your current template. On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks. When you create pictures, charts, or diagrams, they also coordinate with your current document look.
+            This app will be useful for people in buying Fruites. This project is given by RSS. Feeling proud to work for RSS.
         </p>
-         <p> selected text from the Quick Styles gallery on the Home tab. You can also format text directly by using the other controls on the Home tab. Most controls offer a choice of using the look from the current theme or using a format that you specify directly. To change the overall look of your document, choose new Theme elements on the Page Layout tab. To change the looks available in the Quick Style gallery, use the Change Current Quick Style Set command. Both the Themes gallery and the Quick Styles gallery provide reset commands so that you can always restore the look of your document to the original contained in your current template.
+         <p> I have a brilliant girl in my Team named Harika. She can complete sudoku in easy level in 1.20 sec. and hard level 10 to 11 min.
         </p> `
 };
   

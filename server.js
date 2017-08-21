2169ca5a85533fd4b12c68310af02033c4ab1766
SPavanKumar <30950693+SPavanKumar@users.noreply.github.com>
2017-08-21 17:29:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,42 @@ app.get('/',function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var Pro1 = { 
+    title: 'ProRss',
+    heading: 'Mobile app',
+    date : 'aug12,15',
+    content: `<p> Hi this is Pavan Kumar. I am going to develop application for Rss People </p>
+              <p> The app will be very useful for fruite marketing and people well get fruites just with one tap</p>
+              <p> Happy to get this work and feeling proud to serve RSS.</p>`
+};
+
+var htmlTemplat = ` <!DOCTYPE html>
+                    <html>
+                    <head>
+                        
+                        <title>${title}</title>
+                        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                        <link href="/ui/style.css" rel="stylesheet" />
+                    </head>
+                    <body>
+                        <h1>${heading}</h1>
+                        
+                      <div class=abc>
+                            
+                        <div>
+                            <a href="/">Home</a>
+                        </div> 
+                        <hr/>
+                        <div>${date}</div>
+                        <div>
+                          ${content}
+                        </div>
+                       </div>
+                    </body>
+                    </html>
+ 
+`;
+    
 app.get('/ui/style.css',function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

2a14b41184eca0bdb20b062256f2bba1de703157
SPavanKumar <30950693+SPavanKumar@users.noreply.github.com>
2017-08-29 15:25:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var img = document.getElementById("madi");
 var marginLeft = 0;
 function moveRight() {
     marginLeft= marginLeft + 10;
-    img.sytle.marginLeft = marginLeft + 'px';  
+    img.style.marginLeft = marginLeft + 'px';  
 }
 img.onclick = function() {
        var intervel = setIntervel(moveRight, 100);

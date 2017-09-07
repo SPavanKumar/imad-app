f0b8cd4a704a52737a78097197d2b67e8d725d4d
SPavanKumar <30950693+SPavanKumar@users.noreply.github.com>
2017-09-07 17:49:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,6 +58,39 @@ submit.onclick = function () {
      var name = nameInput.value;
      request.open('GET', 'http://kumarpavan284.imad.hasura-app.io/submit-name?name='+ name,true);
      request.send(null); 
+    
+// submit name
+
+var submit = document.getElementById('sub_bt');
+submit.onclick = function () {
+      //create a request variable
+    
+    var request = new XMLHttpRequest();
+    
+    //capture a request and store it in a variable.
+    request.onreadystatechange = function () {
+      if(request.readyState === XMLHttpRequest.DONE) {
+         // Take some action
+         if(request.status===200) {
+              var names = request.responseText;
+              names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i<names.length;i++) {
+                 list+= '<li>'+names[i] + '</li>';
+                 }
+   var ul=document.getElementById('namelist');
+   ul.innerHTML = list;
+         }  
+      }
+      
+    };
+     
+     //make the request
+     var nameInput = document.getElementById('name');
+     var name = nameInput.value;
+     request.open('GET', 'http://kumarpavan284.imad.hasura-app.io/submit-pro1?name='+ name,true);
+     request.send(null); 
+ 
  
  
 };

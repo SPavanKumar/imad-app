4b565994b58b3ef743673eb54ecdce288a3727d1
SPavanKumar <30950693+SPavanKumar@users.noreply.github.com>
2017-09-14 23:50:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ function createTemplate(data) {
 
 app.get('projects/:projectName',function (req, res) {
   
-    pool.query("SELECT * FROM project WHERE title = '" + req.params.projectName+"'",  function (err, result) {
+    pool.query("SELECT * FROM project WHERE title = '" + req.params.projectName + "'",  function (err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {

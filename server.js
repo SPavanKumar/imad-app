237dc7f900bf7fd996fe325738bbb35dd52f82f8
SPavanKumar <30950693+SPavanKumar@users.noreply.github.com>
2017-09-14 17:38:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,6 +114,30 @@ function createTemplate(data) {
                     return htmlTemplate;
 }
 
+
+
+
+
+app.get('projects/:projectName',function (req, res) {
+   
+    pool.query("SELECT * FROM project WHERE title = '" + req.params.projectName+"'",  function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0) {
+                res.status(404).send('project not found');
+            }  else {
+                var projectData = result.rows[0];
+                res.send(createTemplate(projectData));
+            }
+        }  
+        
+    });
+    
+  });
+
+
+
 app.get('/',function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -148,23 +172,6 @@ app.get('/submit-name', function (req, res) {
     
 });
 
-app.get('projects/:projectName',function (req, res) {
-   
-    pool.query("SELECT * FROM project WHERE title = '" + req.params.projectName+"'",  function (err, result) {
-        if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            if (result.rows.length === 0) {
-                res.status(404).send('project not found');
-            }  else {
-                var projectData = result.rows[0];
-                res.send(createTemplate(projectData));
-            }
-        }  
-        
-    });
-    
-  });
 
 
 
